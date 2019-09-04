f1e4a9286e1555134190db98cfb53708c342412f
Matheus Baldi <matheushbaldi@gmail.com>
2019-09-03 21:42:39 -0300

    ESLint installation and usage examples

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const a = 3;
+const b = 4;
+const c = 'jfdsiofjsadiofjfposdijfidsopfjfisodpjfisaopfjdiopsajfidosapfjdiajjjjjjjpdjsifopasjfisdapjfsiaofjpa';
+
+function sum() {
+  return a + b + c;
+}
+
+sum();
