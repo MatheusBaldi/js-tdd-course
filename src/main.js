7a544c4cb1b733ed2f27e75a16508023a466118f
Matheus Baldi <matheushbaldi@gmail.com>
2019-09-03 21:57:37 -0300

    Add .editorconfig; create npm run lint command

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const a = 3;
 const b = 4;
-const c = 'jfdsiofjsadiofjfposdijfidsopfjfisodpjfisaopfjdiopsajfidosapfjdiajjjjjjjpdjsifopasjfisdapjfsiaofjpa';
+const c = 'jfdsiofj';
 
 function sum() {
   return a + b + c;
