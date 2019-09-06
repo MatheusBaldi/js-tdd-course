1d77218b7819d6c773cc986dad5c4dd26b57a6d4
Matheus Baldi <matheushbaldi@gmail.com>
2019-09-05 23:11:18 -0300

    string methods

---
+++ src/string_methods.js
@@ -0,0 +1,10 @@
+const text = 'Lorem ipsum dolot sit amet';
+
+console.log('startsWith', text.startsWith('Lorem'));
+console.log('startsWith', text.startsWith('rem', 2));
+console.log('endsWith', text.endsWith('ame'));
+console.log('endsWith', text.endsWith('ame', 25));
+
+console.log('includes', text.includes('ipsum'));
+
+console.log('repeat', 'lol'.repeat(10));
