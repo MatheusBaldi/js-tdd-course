ee50df03a86369da03b01f370d7b9a62f172c4e1
Matheus Baldi <matheushbaldi@gmail.com>
2019-09-10 14:22:17 -0300

    exemplo com hook beforeEach e possiveis usos para outros hooks

--- js_tdd/tests/main.spec.js
+++ js_tdd/tests/main.spec.js
@@ -1,37 +1,41 @@
 describe('Main', function() {
+  let arr; // define a variávvel no escopo mais alto
 
+  // roda uma vez antes de todos os it
   before(function() {
-    console.log('before');
+    // abre conexão com banco
+    // cria conjunto de dados
   });
 
-
+  // roda uma vez depois de todos os it
   after(function() {
-    console.log('after');
+    // fecha conexão com banco
+    // apaga conjunto de dados
   });
 
+  // roda uma vez antes de cada it
   beforeEach(function() {
-    console.log('before each');
+    arr = [1, 2, 3]; // reseta o valor da variável para cada it
   });
 
+  // roda uma vez depois de cada it
   afterEach(function() {
-    console.log('after each');
   });
 
-  describe('asd', function() {
-    context('case1', function() {
-      it('test 1', function() {
 
-      });
-    });
-    context('case2', function() {
-      it('test 2', function() {
-      });
-    });
+
+  it('should have a size of 4 when pushed another value to the array', function() {
+    arr.push(4);
+    console.log(arr.length); //4
   });
-  describe('asdss', function() {
-    context('case2', function() {
-      it('test 2', function() {
-      });
-    });
+
+  it('should have a size of 2 when a value is popped from the array', function() {
+    arr.pop();
+    console.log(arr.length); //2
   });
+
+  it('should remove 3 when use pop in the array', function(){
+    console.log(arr.pop() === 3); //true
+  });
+
 });
