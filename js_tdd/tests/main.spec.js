f01ce8dcc8c0ec60d8ae50d6303126f1c94ea775
Matheus Baldi <matheushbaldi@gmail.com>
2019-09-10 14:09:32 -0300

    entedendo hooks before, after, beforeEach, afterEach

--- js_tdd/tests/main.spec.js
+++ js_tdd/tests/main.spec.js
@@ -1,24 +1,37 @@
 describe('Main', function() {
-  describe('Method A', function() {
-    context('Case 1', function() {
-      it('should happen x', function() {
-        throw new Error('just an error');
+
+  before(function() {
+    console.log('before');
+  });
+
+
+  after(function() {
+    console.log('after');
+  });
+
+  beforeEach(function() {
+    console.log('before each');
+  });
+
+  afterEach(function() {
+    console.log('after each');
+  });
+
+  describe('asd', function() {
+    context('case1', function() {
+      it('test 1', function() {
+
       });
     });
-
-    context.skip('Case 2', function() {
-      it('should happen x', function() {
-        //
-        throw new Error('just an error');
-
-      });
-      it('should happen vdsvds', function() {
-        //
+    context('case2', function() {
+      it('test 2', function() {
       });
     });
   });
-
-  describe('Method B', function() {
-
+  describe('asdss', function() {
+    context('case2', function() {
+      it('test 2', function() {
+      });
+    });
   });
 });
